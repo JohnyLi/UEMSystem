f4f52962cea6bfb727c84d5cd2b52abd34566de7
ZOUZIJING-AI-ASSIGNMENT <34477314+zouzijing@users.noreply.github.com>
2018-12-05 15:11:02 +0800

    2333

--- service/api/getAllAccountInfo.py
+++ service/api/getAllAccountInfo.py
@@ -1,4 +1,20 @@
-
-
+from flask import request,jsonify
+import json
+from startService import getSQLHandler
+from util.Sha import *
 def getAllAccountInfoImpl():
-    return []
+    response = {}
+    handler = getSQLHandler()
+    myjson = json.loads(request.get_data().decode())
+    name = myjson['name']
+    password = myjson['password']
+    SHA_PW = get_sha(password)
+    result = handler.SELECT("select * from login where name=%s",(name))
+    if len(result)==0:
+        response['status']="用户名不存在"
+    else:
+        if SHA_PW==result[0][2]:
+            response['status']="登陆成功"
+        else:
+            response['status']="密码错误"
+    return jsonify(response)
--- startService.py
+++ startService.py
@@ -64,4 +64,4 @@ def api_getAllAccountInfo():
 
 if __name__ == '__main__':
     sqlServer.start()
-    app.run(port=5000,debug=True)
+    app.run(port=80,debug=True)
