2f8f9b8c3a066b50bb83d77b0017802fe38bf624
ZOUZIJING-AI-ASSIGNMENT <34477314+zouzijing@users.noreply.github.com>
2018-12-19 16:28:55 +0800

    2333

--- service/web/web_student_management.py
+++ service/web/web_student_management.py
@@ -2,6 +2,7 @@
 from flask import render_template,redirect,url_for,session
 from util.Functions1 import getSQLHandler,GetSideBar,check_session
 from conf.Config import *
+from Dao.SELECT.major import GET_ALL_major_INFOS
 def web_student_managementImpl():
     if not check_session():
         return redirect(url_for("logout"))
@@ -11,5 +12,5 @@ def web_student_managementImpl():
 
     sidebar = GetSideBar(ADMIN_BAR,"学生管理")
     handler = getSQLHandler()
-
-    return render_template("student_management.html",sidebar=sidebar,url_dict=url_dict)
+    majors = GET_ALL_major_INFOS(handler)
+    return render_template("student_management.html",sidebar=sidebar,url_dict=url_dict,majors=majors)
